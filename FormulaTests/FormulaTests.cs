adff6a66d61f6fb6b4a81e41215e659cf5399051
MATTHEW GOH <u1365516@umail.utah.edu>
2023-02-03 00:48:33 -0700

    Added more tests

--- FormulaTests/FormulaTests.cs
+++ FormulaTests/FormulaTests.cs
@@ -6,6 +6,7 @@ namespace FormulaTests
     [TestClass]
     public class FormulaTests
     {
+        // Test evaluate method with basic operations
         [TestMethod]
         public void TestSimpleAddition()
         {
@@ -28,11 +29,58 @@ namespace FormulaTests
         }
 
         [TestMethod]
+        public void TestOrderOfOperations2()
+        {
+            Formula f = new Formula("(13-9) / 2 *(5 +2)");
+            Assert.AreEqual((double)14, f.Evaluate(x => 6));
+        }
+
+        [TestMethod]
+        public void TestLegalParenthesis()
+        {
+            Formula f = new Formula("(13)-9 / (2) *5 +2");
+            Assert.AreEqual((double)-7.5, f.Evaluate(x => 6));
+        }
+
+        // Tests for all the exceptions related to the input
+        [TestMethod]
         [ExpectedException(typeof(FormulaFormatException),
         "Exception when operators as start of formula")]
-        public void TestFormulaException()
+        public void TestFormulaExceptionNegative()
         {
             Formula f = new Formula("-5 * 3");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException),
+        "Exception when operators as start of formula")]
+        public void TestFormulaExceptionPar()
+        {
+            Formula f = new Formula("(5) * 3)");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException),
+        "Exception when operators as start of formula")]
+        public void TestFormulaExceptionPar2()
+        {
+            Formula f = new Formula("(5 * 3");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException),
+        "Exception when operators as start of formula")]
+        public void TestFormulaExceptionPar3()
+        {
+            Formula f = new Formula(")5(3)");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException),
+        "Exception when operators as start of formula")]
+        public void TestFormulaExceptionPar4()
+        {
+            Formula f = new Formula("5(12)");
         }
     }
 }
