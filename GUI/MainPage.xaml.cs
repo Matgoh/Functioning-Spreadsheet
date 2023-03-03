1ada63b5b7d51a4d6e6c632b78d14da8667c49cd
MATTHEW GOH <u1365516@umail.utah.edu>
2023-03-03 05:09:20 -0700

    Letters and numbers to axis

--- GUI/MainPage.xaml.cs
+++ GUI/MainPage.xaml.cs
@@ -1,7 +1,4 @@
-﻿using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Vml;
-using SS;
-using System.Text;
+﻿using SS;
 
 namespace GUI
 {
@@ -13,7 +10,82 @@ namespace GUI
         public MainPage()
         {
             InitializeComponent();
+
+            Char c = 'A';
             for (int i = 0; i < 26; i++)
+            {
+                label = c;
+                TopLabels.Add(
+                  new Border
+                  {
+                      Stroke = Color.FromRgb(0, 0, 0),
+                      StrokeThickness = 1,
+                      HeightRequest = 20,
+                      WidthRequest = 75,
+                      HorizontalOptions = LayoutOptions.Center,
+                      Content =
+                          new Label
+                          {
+                              Text = $"{label}",
+                              BackgroundColor = Color.FromRgb(200, 200, 250),
+                              HorizontalTextAlignment = TextAlignment.Center
+                          }
+                  }
+                  );
+                c++;
+            }
+            int num = 1;
+            for (int i = 0; i < 99; i++)
+            {
+                label = num;
+                LeftLabels.Add(
+                  new Border
+                  {
+                      Stroke = Color.FromRgb(0, 0, 0),
+                      StrokeThickness = 1,
+                      HeightRequest = 20,
+                      WidthRequest = 75,
+                      HorizontalOptions = LayoutOptions.Center,
+                      Content =
+                          new Label
+                          {
+                              Text = $"{label}",
+                              BackgroundColor = Color.FromRgb(200, 200, 250),
+                              HorizontalTextAlignment = TextAlignment.Center
+                          }
+                  }
+                  );
+                num++;
+            }
+
+            label = null;
+            for (int i = 0; i < 52; i++)
+            {
+                GridH.Add(
+                new Border
+                {
+                    Stroke = Color.FromRgb(0, 0, 0),
+                    StrokeThickness = 1,
+                    HeightRequest = 20,
+                    WidthRequest = 75,
+                    HorizontalOptions = LayoutOptions.Center,
+                    Content =
+                         new Label
+                         {
+                            Text = $"{label}",
+                            BackgroundColor = Color.FromRgb(200, 200, 250),
+                            HorizontalTextAlignment = TextAlignment.Center
+                         }
+                }
+                );              
+               
+            }
+        }
+
+
+        void FileMenuNew(object sender, EventArgs args)
+        {
+            for (int i = 0; i < 10; i++)
             {
                 TopLabels.Add(
                   new Border
@@ -32,8 +104,14 @@ namespace GUI
                           }
                   }
                   );
+            }
+        }
 
-                LeftLabels.Add(
+        void FileMenuOpenAsync(object sender, EventArgs args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                TopLabels.Add(
                   new Border
                   {
                       Stroke = Color.FromRgb(0, 0, 0),
@@ -51,36 +129,13 @@ namespace GUI
                   }
                   );
             }
+        }
 
-            async void FileMenuNew(object sender, EventArgs args)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    TopLabels.Add(
-                      new Border
-                      {
-                          Stroke = Color.FromRgb(0, 0, 0),
-                          StrokeThickness = 1,
-                          HeightRequest = 20,
-                          WidthRequest = 75,
-                          HorizontalOptions = LayoutOptions.Center,
-                          Content =
-                              new Label
-                              {
-                                  Text = $"{label}",
-                                  BackgroundColor = Color.FromRgb(200, 200, 250),
-                                  HorizontalTextAlignment = TextAlignment.Center
-                              }
-                      }
-                      );
-                }
-            }
+        void OnSaveFile(object sender, EventArgs args)
+        {
 
-            async void OnSaveFile(object sender, EventArgs args)
-            {
-                Spreadsheet spreadsheet = new Spreadsheet();
-                spreadsheet.Save("hello");
-            }
+            Spreadsheet spreadsheet = new Spreadsheet();
+            spreadsheet.Save("hello");
         }
     }
 }
